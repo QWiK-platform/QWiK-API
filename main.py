17e5e0a0a86e3565df168ab4ce31687f9aef06b2
mooonu <moonudev@gmail.com>
2025-12-27 17:15:06 +0900

    CORS 추가

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://qw1k.cloud",
 ]
 
 app.add_middleware(
